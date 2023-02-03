bc42bba634b314d0309981835ebf2216ffb06b21
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-02-03 17:14:36 +0100

    adaptado el registro

--- app/src/main/java/com/example/intermodular/register/data/network/RegisterService.kt
+++ app/src/main/java/com/example/intermodular/register/data/network/RegisterService.kt
@@ -1,6 +1,7 @@
 package com.example.intermodular.register.data.network
 
 import com.example.intermodular.core.network.RetrofitHelper
+import com.example.intermodular.register.data.dto.UserDTO
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -9,7 +10,7 @@ class RegisterService {
 
     suspend fun doRegister(name : String, user: String, email : String, password : String): Boolean {
         return withContext(Dispatchers.IO){
-            val response = retrofit.create(RegisterClient::class.java).doRegister(name, user, email, password)
+            val response = retrofit.create(RegisterClient::class.java).doRegister(UserDTO(name, user, email, password))
 
             return@withContext response.body()?.success == true
         }
