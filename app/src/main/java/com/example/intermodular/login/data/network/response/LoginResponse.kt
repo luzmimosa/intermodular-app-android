e67327edb014a67b7b09b76583cd44a505d778da
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-02-09 16:47:07 +0100

    cambiados los nombres por consistencia

--- app/src/main/java/com/example/intermodular/login/data/network/response/LoginResponse.kt
+++ app/src/main/java/com/example/intermodular/login/data/network/response/LoginResponse.kt
@@ -3,6 +3,6 @@ package com.example.intermodular.login.data.network.response
 import com.google.gson.annotations.SerializedName
 
 data class LoginResponse(
-    @SerializedName("ok")
-    val loginOk: Boolean
+    @SerializedName("message")
+    val loginResponse: String
 )
