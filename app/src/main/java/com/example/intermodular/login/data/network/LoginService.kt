233a99983b136074f4a7051cb879fd39157aaf02
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-02-09 16:46:47 +0100

    el login es ahora funcional, y envía el token recibido a donde el argumento especificado diga

--- app/src/main/java/com/example/intermodular/login/data/network/LoginService.kt
+++ app/src/main/java/com/example/intermodular/login/data/network/LoginService.kt
@@ -1,17 +1,24 @@
 package com.example.intermodular.login.data.network
 
 import com.example.intermodular.core.network.RetrofitHelper
+import com.example.intermodular.login.data.dto.LoginDTO
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import okhttp3.Dispatcher
 
 class LoginService {
     private val retrofit= RetrofitHelper.getRetrofit()
 
-    suspend fun doLogin(user: String, password: String): Boolean{
+    suspend fun doLogin(user: String, password: String, sendTokenTo: (token: String) -> Unit): Boolean{
         return withContext(Dispatchers.IO){
-            val response= retrofit.create(LoginClient::class.java).doLogin()
-            response.body()?.loginOk ?: false
+            val response= retrofit.create(LoginClient::class.java).doLogin(LoginDTO(user, password))
+
+            val loginSuccessful = response.isSuccessful
+
+            if (loginSuccessful) {
+                sendTokenTo(response.headers().get("Authorization") ?: "")
+            }
+
+            return@withContext loginSuccessful
         }
     }
 }
