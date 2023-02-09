606f3dfdbb505552ccb65522c20337ea6e3cd6fa
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-02-09 16:48:30 +0100

    ahora especifica dónde enviar el token una vez recibido

--- app/src/main/java/com/example/intermodular/login/domain/LoginUseCase.kt
+++ app/src/main/java/com/example/intermodular/login/domain/LoginUseCase.kt
@@ -1,11 +1,16 @@
 package com.example.intermodular.login.domain
 
+import android.util.Log
+import com.example.intermodular.core.authentication.AuthenticationTokenManager
 import com.example.intermodular.login.data.LoginRepository
 
 class LoginUseCase {
     private val repository= LoginRepository()
 
     suspend operator fun invoke(user: String, password: String): Boolean{
-        return repository.doLogin(user, password)
+        return repository.doLogin(user, password) {
+            token -> AuthenticationTokenManager.token = token
+            Log.i("WikiHonk debug", "Token: $token")
+        }
     }
 }
