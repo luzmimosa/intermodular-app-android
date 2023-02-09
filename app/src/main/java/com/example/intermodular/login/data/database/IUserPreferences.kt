604a33d4342a0311a217452e3a31e7db55f198b0
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-02-09 20:59:41 +0100

    ahora sí fufa :D

--- app/src/main/java/com/example/intermodular/login/data/database/IUserPreferences.kt
+++
@@ -1,6 +0,0 @@
-package com.example.intermodular.login.data.database
-
-interface IUserPreferences {
-    suspend fun addToken(key: String, value: String)
-    suspend fun getToken(key: String) : String
-}
--- app/src/main/java/com/example/intermodular/map/ui/MapScreen.kt
+++ app/src/main/java/com/example/intermodular/map/ui/MapScreen.kt
@@ -4,10 +4,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.key
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.intermodular.R
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.CameraPositionState
@@ -28,7 +31,8 @@ fun MapScreen(mapViewModel: MapViewModel, navigationController: NavController){
     ){
         GoogleMap(
             modifier= Modifier.fillMaxSize(),
-            cameraPositionState= CameraPositionState()
+            cameraPositionState= CameraPositionState(),
+
         ){
             Polyline(points = listOf(
                 LatLng(38.55359897196608, -0.12057169825429333),
