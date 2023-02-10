2fdfa0249f29ee098e2d38bfe738ea7481b106c4
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-02-10 18:51:05 +0100

    implementada la ruta de ejemplo

--- app/src/main/java/com/example/intermodular/map/ui/MapScreen.kt
+++ app/src/main/java/com/example/intermodular/map/ui/MapScreen.kt
@@ -4,13 +4,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.key
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.intermodular.R
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.CameraPositionState
@@ -25,23 +24,24 @@ fun MapScreen(mapViewModel: MapViewModel, navigationController: NavController){
         position = CameraPosition.fromLatLngZoom(localizacion, 13f)
     }
 
+    val route by mapViewModel.route.observeAsState(null)
+
     Box(modifier= Modifier
         .fillMaxSize()
         .padding(8.dp)
     ){
-        GoogleMap(
-            modifier= Modifier.fillMaxSize(),
-            cameraPositionState= CameraPositionState(),
+        if (route != null) {
+            GoogleMap(
+                modifier= Modifier.fillMaxSize(),
+                cameraPositionState= CameraPositionState(),
 
-        ){
-            Polyline(points = listOf(
-                LatLng(38.55359897196608, -0.12057169825429333),
-                LatLng(39.55359897196608, -0.12057169825429333),
-                LatLng(40.55359897196608, -0.12057169825429333),
-                LatLng(41.55359897196608, -0.12057169825429333)
-            ),
-                color= Color.Red
-            )
+                ){
+                Polyline(
+                    points = if (route == null) arrayListOf() else route!!.locations.map { LatLng(it.latitude, it.longitude) },
+                    width= 10f,
+                    color= Color.Red
+                )
+            }
         }
     }
 }
