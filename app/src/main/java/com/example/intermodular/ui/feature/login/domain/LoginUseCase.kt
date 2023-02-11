e6d94e8a4c72f5002fd907943354362a5eeb40e7
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-02-12 00:15:17 +0100

    implementada la clase LoginResponse como respuesta

--- app/src/main/java/com/example/intermodular/ui/feature/login/domain/LoginUseCase.kt
+++ app/src/main/java/com/example/intermodular/ui/feature/login/domain/LoginUseCase.kt
@@ -3,11 +3,12 @@ package com.example.intermodular.ui.feature.login.domain
 import android.util.Log
 import com.example.intermodular.core.authentication.AuthenticationTokenManager
 import com.example.intermodular.ui.feature.login.data.LoginRepository
+import com.example.intermodular.ui.feature.login.data.network.LoginResult
 
 class LoginUseCase {
     private val repository= LoginRepository()
 
-    suspend operator fun invoke(user: String, password: String): Boolean{
+    suspend operator fun invoke(user: String, password: String): LoginResult {
         return repository.doLogin(user, password) {
             token -> AuthenticationTokenManager.token = token
             Log.i("WikiHonk debug", "Token: $token")
