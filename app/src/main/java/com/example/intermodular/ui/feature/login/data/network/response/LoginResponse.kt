90950aecfedbd54cfd6beb17eea18536f73e90db
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-02-12 00:14:38 +0100

    reformateado por consistencia

--- app/src/main/java/com/example/intermodular/ui/feature/login/data/network/response/LoginResponse.kt
+++ app/src/main/java/com/example/intermodular/ui/feature/login/data/network/response/LoginResponse.kt
@@ -3,6 +3,5 @@ package com.example.intermodular.ui.feature.login.data.network.response
 import com.google.gson.annotations.SerializedName
 
 data class LoginResponse(
-    @SerializedName("message")
-    val loginResponse: String
+    @SerializedName("message") val message: String
 )
