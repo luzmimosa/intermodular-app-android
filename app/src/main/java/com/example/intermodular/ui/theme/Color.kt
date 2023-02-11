4c66c7ab053163336cb4d77ba2e29a38627d59a3
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-02-11 14:05:42 +0100

    establecidos los colores del tema claro y del tema oscuro

--- app/src/main/java/com/example/intermodular/ui/theme/Color.kt
+++ app/src/main/java/com/example/intermodular/ui/theme/Color.kt
@@ -2,19 +2,39 @@ package com.example.intermodular.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+// Light palette colors
+val lightPrimary = Color(0xFFE0E0E0)
+val lightPrimaryVariant = Color(0xFFFFFFFF)
 
-val verde1= Color(0xff036d19)
-val verde2= Color(0xff09a129)
-val verde3= Color(0xff12ba58)
-val verde4= Color(0xff27fb6b)
-val azulmarino= Color(0xff0a2e36)
-val marron1= Color(0xff543d27)
-val marron2= Color(0xff7e5c3a)
-val marron3= Color(0xffb98350)
-val marron4= Color(0xCA9B68)
-val gris1= Color(0xff6e6e6e)
-val gris2= Color(0xffc0babc)
+val lightSecondary = Color(0xFF00B0FF)
+val lightSecondaryVariant = Color(0xFF0091EA)
+
+val lightBackground = Color(0xFF28A85B)
+val lightSurface = Color(0xFF049440)
+
+val lightError = Color(0xFFB00020)
+
+val lightOnPrimary = Color(0xFF0D1A00)
+val lightOnSecondary = Color(0xFF0D1A00)
+val lightOnBackground = Color(0xFF0D1A00)
+val lightOnSurface = Color(0xFF0D1A00)
+val lightOnError = Color(0xFFFFFFFF)
+
+
+// Dark palette colors
+val darkPrimary = Color(0xFF121212)
+val darkPrimaryVariant = Color(0xFF1E1E1E)
+
+val darkSecondary = Color(0xFF174052)
+val darkSecondaryVariant = Color(0xFF002C3A)
+
+val darkBackground = Color(0xFF1B5030)
+val darkSurface = Color(0xFF003300)
+
+val darkError = Color(0xFFCF6679)
+
+val darkOnPrimary = Color(0xFF9BE799)
+val darkOnSecondary = Color(0xFF9BE799)
+val darkOnBackground = Color(0xFF9BE799)
+val darkOnSurface = Color(0xFF9BE799)
+val darkOnError = Color(0xFF1B251B)
