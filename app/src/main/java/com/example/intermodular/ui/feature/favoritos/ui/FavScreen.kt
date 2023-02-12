f5fd3d93df1047067428ed93c99d05586c7da0aa
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-02-12 16:53:43 +0100

    importado el paquete de componentes desde su nueva ubicación

--- app/src/main/java/com/example/intermodular/ui/feature/favoritos/ui/FavScreen.kt
+++ app/src/main/java/com/example/intermodular/ui/feature/favoritos/ui/FavScreen.kt
@@ -3,8 +3,8 @@ package com.example.intermodular.ui.feature.favoritos.ui
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
-import com.example.intermodular.ui.component.WikihonkBottomBar
-import com.example.intermodular.ui.component.WikihonkTopBar
+import com.example.intermodular.ui.component.global.WikihonkBottomBar
+import com.example.intermodular.ui.component.global.WikihonkTopBar
 
 @Composable
 fun Fav(favViewModel: FavViewModel, navigationController: NavHostController){
--- app/src/main/java/com/example/intermodular/ui/feature/inforuta/InfoRutaScreen.kt
+++ app/src/main/java/com/example/intermodular/ui/feature/inforuta/InfoRutaScreen.kt
@@ -3,8 +3,8 @@ package com.example.intermodular.ui.feature.inforuta
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
-import com.example.intermodular.ui.component.WikihonkBottomBar
-import com.example.intermodular.ui.component.WikihonkTopBar
+import com.example.intermodular.ui.component.global.WikihonkBottomBar
+import com.example.intermodular.ui.component.global.WikihonkTopBar
 
 @Composable
 fun InfoRuta(infoRutaViewModel: InfoRutaViewModel, navigationController: NavHostController){
