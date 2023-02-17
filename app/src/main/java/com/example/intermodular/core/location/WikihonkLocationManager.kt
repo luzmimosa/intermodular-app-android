a6d486d2917561286fe50f642b274a90d7075aee
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-02-17 14:32:32 +0100

    el proveedor de localizaciones ya es funcional. Además, ahora es un objecto único que se tiene que inicializar.

--- app/src/main/java/com/example/intermodular/core/location/WikihonkLocationManager.kt
+++ app/src/main/java/com/example/intermodular/core/location/WikihonkLocationManager.kt
@@ -3,24 +3,23 @@ package com.example.intermodular.core.location
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.HandlerThread
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.google.android.gms.location.Granularity
-import com.google.android.gms.location.LocationListener
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.tasks.Task
 
-class WikihonkLocationManager {
+object WikihonkLocationManager {
 
     private val _userLocation = MutableLiveData<LocationMark>()
     val userLocation: LiveData<LocationMark> = _userLocation
 
-    private var listener: Task<Void>? = null
+    private var locationListener: LocationListener? = null
+    private val locationHandlerThread = HandlerThread("Location listener thread")
 
     fun startListening(context: Context) {
-        if (listener != null) {
+        if (locationListener != null) {
             return
         }
 
@@ -35,25 +34,27 @@ class WikihonkLocationManager {
             throw Exception("Location permission not granted")
         }
 
-        this.listener = LocationServices
-            .getFusedLocationProviderClient(context)
-            .requestLocationUpdates(
-                LocationRequest.Builder(2500)
-                    .setMaxUpdates(Integer.MAX_VALUE)
-                    .setMinUpdateDistanceMeters(4f)
-                    .setGranularity(Granularity.GRANULARITY_FINE)
-                    .build(),
-                {
+        val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
-                },
-                { location ->
-                    _userLocation.value = LocationMark(
-                        latitude = location.latitude,
-                        longitude = location.longitude
-                    )
-                }
-        )
+        locationListener().let {
+            this.locationListener = it
+            locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                100L,
+                1f,
+                it,
+                locationHandlerThread.looper
+            )
+        }
     }
+
+    private fun locationListener(): LocationListener =
+        LocationListener { newLocation ->
+            _userLocation.value = LocationMark(
+                latitude = newLocation.latitude,
+                longitude = newLocation.longitude
+            )
+        }
 }
 
 
