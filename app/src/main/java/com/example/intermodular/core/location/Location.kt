53b543faa76165f459b5ce321d19501532cb1919
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-02-17 14:31:22 +0100

    añadida función toString para usos de depuración

--- app/src/main/java/com/example/intermodular/core/location/Location.kt
+++ app/src/main/java/com/example/intermodular/core/location/Location.kt
@@ -1,9 +1,14 @@
 package com.example.intermodular.core.location
 
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 data class LocationMark(
     val latitude: Double,
     val longitude: Double,
     val dateTime: LocalDateTime = LocalDateTime.now()
-)
+) {
+    override fun toString(): String {
+        return "LocationMark(latitude=$latitude, longitude=$longitude, dateTime=${dateTime.format(DateTimeFormatter.ISO_DATE_TIME)})"
+    }
+}
