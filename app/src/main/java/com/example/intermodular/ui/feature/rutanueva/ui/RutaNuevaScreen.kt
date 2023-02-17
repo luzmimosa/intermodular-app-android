514ab9cf98afb3b13f4f8441a30ff4b540186549
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-02-17 15:16:25 +0100

    añadida la funcionalidad de tomar una foto para la ruta

--- app/src/main/java/com/example/intermodular/ui/feature/rutanueva/ui/RutaNuevaScreen.kt
+++ app/src/main/java/com/example/intermodular/ui/feature/rutanueva/ui/RutaNuevaScreen.kt
@@ -1,31 +1,31 @@
 package com.example.intermodular.ui.feature.rutanueva.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import com.example.intermodular.R
+import com.example.intermodular.core.camera.CameraManager
 import com.example.intermodular.ui.component.global.ClickableText
 import com.example.intermodular.ui.component.global.WikihonkBottomBar
 import com.example.intermodular.ui.component.global.WikihonkTopBar
 
-@Preview
 @Composable
-fun RutaNueva(rutanuevaviewmodel: RutaNuevaViewModel= RutaNuevaViewModel(), navigationController: NavHostController= rememberNavController()){
-
-    val nombre: String //by RutaNuevaViewModel.nombre.observeAsState(initial= "")
-    val descripcion: String
-    val dificultad: String
+fun RutaNueva(
+    rutaNuevaViewModel: RutaNuevaViewModel = RutaNuevaViewModel(),
+    navigationController: NavHostController = rememberNavController()
+){
+    val routeMainImage: ImageBitmap? by rutaNuevaViewModel.mainImageBitmap.observeAsState(initial = null)
 
 
     Scaffold(
@@ -48,14 +48,18 @@ fun RutaNueva(rutanuevaviewmodel: RutaNuevaViewModel= RutaNuevaViewModel(), navi
 
                 Row(modifier= Modifier
                     .fillMaxWidth()
-                    .paint(painterResource(id = R.drawable.black))){
-
+                ){
+                    if (routeMainImage != null) {
+                        Image(bitmap = routeMainImage!!, contentDescription = "Imagen de la ruta")
+                    }
                 }
 
                 Row(){
                     Box(modifier= Modifier.padding(115.dp, 10.dp, 10.dp, 10.dp)){
                         ClickableText(text = "Añadir imagen", fontSize = 20.sp, fontWeight = FontWeight.Normal, underlined = true) {
-                            //añadir una imagen de la galeria o que haya subida al servidor
+                            CameraManager.takePicture { image ->
+                                rutaNuevaViewModel.setMainImage(image)
+                            }
                         }
                     }
                 }
--- app/src/main/java/com/example/intermodular/ui/feature/rutanueva/ui/RutaNuevaViewModel.kt
+++ app/src/main/java/com/example/intermodular/ui/feature/rutanueva/ui/RutaNuevaViewModel.kt
@@ -1,18 +1,11 @@
 package com.example.intermodular.ui.feature.rutanueva.ui
 
-import android.util.Log
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import androidx.navigation.NavHostController
-import com.example.intermodular.model.Routes
-import com.example.intermodular.ui.feature.rutanueva.domain.usecase.RutaNuevaUseCase
-import kotlinx.coroutines.launch
 
 class RutaNuevaViewModel: ViewModel() {
-
-    private val rutaNuevaUseCase= RutaNuevaUseCase()
 
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> = _isLoading
@@ -35,72 +28,10 @@ class RutaNuevaViewModel: ViewModel() {
     private val _dificultadAlertVisible= MutableLiveData<Boolean>()
     val dificultadAlertVisible: LiveData<Boolean> = _dificultadAlertVisible
 
-    /*private val _tipoAlertVisible= MutableLiveData<Boolean>()
-    val tipoAlertVisible: LiveData<Boolean> = _tipoAlertVisible*/
+    private val _mainImageBitmap = MutableLiveData<ImageBitmap?>()
+    val mainImageBitmap: LiveData<ImageBitmap?> = _mainImageBitmap
 
-    fun onRegisterChanged(nombre: String, descripcion: String, dificultad: String) {
-        _nombre.value = nombre
-        _descripcion.value = descripcion
-        _dificultad.value = dificultad
-    }
-
-    private fun updateAlerts() {
-        _nombreAlertVisible.value =              !isValidNombre(nombre.value ?: "")
-        _descripcionAlertVisible.value =           !isValidDescripcion(descripcion.value ?: "")
-        _dificultadAlertVisible.value =               !isValidDificultad(dificultad.value ?: "")
-    }
-
-    private fun isValidNombre(nombre: String): Boolean {
-        return nombre.isNotEmpty()
-    }
-
-    private fun isValidDescripcion(descripcion: String): Boolean {
-        return descripcion.isNotEmpty()
-    }
-
-    private fun isValidDificultad(dificultad: String): Boolean {
-        return dificultad.isNotEmpty()
-    }
-
-    private fun isValidRuta(): Boolean {
-        return try {
-            (
-                    isValidNombre(nombre.value!!)
-                            && isValidDescripcion(descripcion.value!!)
-                            && isValidDificultad(dificultad.value!!)
-                    )
-        } catch (e: Exception) {
-            false
-        }
-    }
-
-    fun onButtonCrearRutaPress(navigationController: NavHostController) {
-
-        if (!isValidRuta()) {
-            updateAlerts()
-            return
-        }
-
-        viewModelScope.launch {
-            _isLoading.value = true
-            val result = rutaNuevaUseCase(nombre.value!!, descripcion.value!!, dificultad.value!!)
-
-            if(result) {
-                onCrearRutaOk(navigationController)
-            } else {
-                onCrearRutaError()
-            }
-            _isLoading.value = false
-        }
-    }
-
-    fun onCrearRutaOk(navigationController: NavHostController) {
-        Log.i("WikiHonk", "Ruta creada")
-
-        navigationController.navigate(Routes.HomeScreen.route)
-    }
-
-    fun onCrearRutaError() {
-        Log.i("WikiHonk", "Ruta error")
+    fun setMainImage(imageBitmap: ImageBitmap?) {
+        _mainImageBitmap.value = imageBitmap
     }
 }
