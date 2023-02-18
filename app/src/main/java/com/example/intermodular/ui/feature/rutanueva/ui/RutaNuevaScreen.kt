9d0d4e2fb2b13558e32fb6ccd594e974597cd529
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-02-18 04:41:08 +0100

    añadida funcionalidad

--- app/src/main/java/com/example/intermodular/ui/feature/rutanueva/ui/RutaNuevaScreen.kt
+++ app/src/main/java/com/example/intermodular/ui/feature/rutanueva/ui/RutaNuevaScreen.kt
@@ -22,36 +22,46 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import com.example.intermodular.ui.component.global.WikihonkBaseScreen
 import com.example.intermodular.R
 import com.example.intermodular.core.camera.CameraManager
 import com.example.intermodular.core.route.model.RouteDifficulty
 import com.example.intermodular.core.route.model.RouteType
-import com.example.intermodular.ui.component.global.ErrorSupporterTextField
-import com.example.intermodular.ui.component.global.InputPopup
-import com.example.intermodular.ui.component.global.PredefinedSpacer
+import com.example.intermodular.ui.component.global.*
 
 @Preview
 @Composable
 fun RutaNueva(
-    rutaNuevaViewModel: RutaNuevaViewModel = RutaNuevaViewModel(),
+    rutaNuevaViewModel: RutaNuevaViewModel = RutaNuevaViewModel(navigationController = rememberNavController()),
     navigationController: NavHostController = rememberNavController()
 ){
-    val routeName: String by rutaNuevaViewModel.routeName.observeAsState(initial = stringResource(id = R.string.newroute_default_name))
-    val routeDescription: String by rutaNuevaViewModel.routeDescription.observeAsState(initial = "")
-    val routeDifficulty: RouteDifficulty by rutaNuevaViewModel.routeDifficulty.observeAsState(initial = RouteDifficulty.EASY)
-    val routeMainImage: ImageBitmap? by rutaNuevaViewModel.mainImageBitmap.observeAsState(initial = null)
-    val routeTypes: List<RouteType> by rutaNuevaViewModel.routeTypes.observeAsState(initial = listOf())
+    val routeName: String                       by rutaNuevaViewModel.routeName.observeAsState(initial = stringResource(id = R.string.newroute_default_name))
+    val routeDescription: String                by rutaNuevaViewModel.routeDescription.observeAsState(initial = "")
+    val routeDifficulty: RouteDifficulty?        by rutaNuevaViewModel.routeDifficulty.observeAsState(initial = null)
+    val routeMainImage: ImageBitmap?            by rutaNuevaViewModel.mainImageBitmap.observeAsState(initial = null)
+    val routeTypes: List<RouteType>             by rutaNuevaViewModel.routeTypes.observeAsState(initial = listOf())
 
     // Errors
-    val routeDescriptionErrorMessage: Int by rutaNuevaViewModel.routeDescriptionErrorMessage.observeAsState(initial = R.string.unknown_error_message)
-    val routeDescriptionErrorVisible: Boolean by rutaNuevaViewModel.routeDescriptionErrorVisible.observeAsState(initial = false)
+    val routeDescriptionErrorVisible: Boolean   by rutaNuevaViewModel.routeDescriptionErrorVisible.observeAsState(initial = false)
+    val routeTypesErrorVisible: Boolean         by rutaNuevaViewModel.routeTypesErrorVisible.observeAsState(initial = false)
+    val routeDifficultyErrorVisible: Boolean    by rutaNuevaViewModel.routeDifficultyErrorVisible.observeAsState(initial = false)
+    val routeImageErrorPopupVisible: Boolean    by rutaNuevaViewModel.routeImageErrorVisible.observeAsState(initial = false)
 
     // Popups
-    val routeNamePopupVisible: Boolean by rutaNuevaViewModel.routeNamePopupVisible.observeAsState(initial = false)
-    val routeDifficultyPopupVisible: Boolean by rutaNuevaViewModel.routeDifficultyPopupVisible.observeAsState(initial = false)
+    val routeNamePopupVisible: Boolean          by rutaNuevaViewModel.routeNamePopupVisible.observeAsState(initial = false)
+    val routeDifficultyPopupVisible: Boolean    by rutaNuevaViewModel.routeDifficultyPopupVisible.observeAsState(initial = false)
+    val waypointCreatedPopupVisible: Boolean    by rutaNuevaViewModel.waypointCreatedPopupVisible.observeAsState(initial = false)
+
+    // Waypoint builder
+    val waypointBuilderVisible: Boolean         by rutaNuevaViewModel.waypointPopupVisible.observeAsState(initial = false)
+    val waypointName: String                    by rutaNuevaViewModel.waypointName.observeAsState(initial = "")
+    val waypointDescription: String             by rutaNuevaViewModel.waypointDescription.observeAsState(initial = "")
+    val waypointImage: ImageBitmap?             by rutaNuevaViewModel.waypointImage.observeAsState(initial = null)
+
+    val recordingState: RecordingState          by rutaNuevaViewModel.recordingState.observeAsState(initial = RecordingState.READY)
 
 
     WikihonkBaseScreen(navigationController = navigationController) {
@@ -92,7 +102,7 @@ fun RutaNueva(
                     ) {
                         Descripcion(
                             value = routeDescription,
-                            errorMessage = stringResource(id = routeDescriptionErrorMessage),
+                            errorMessage = stringResource(id = R.string.newroute_description_error_message),
                             showError = routeDescriptionErrorVisible,
                             onValueChange = {
                                 rutaNuevaViewModel.setDescription(it)
@@ -119,6 +129,12 @@ fun RutaNueva(
                         )
                     }
 
+                    if (routeDifficultyErrorVisible) {
+                        Row {
+                            ErrorText(text = stringResource(id = R.string.newroute_difficulty_error))
+                        }
+                    }
+
                     Row { PredefinedSpacer() }
 
                     Row {
@@ -131,6 +147,45 @@ fun RutaNueva(
                                 rutaNuevaViewModel.removeRouteType(type)
                             }
                         )
+                    }
+                }
+            }
+            
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(10.dp)
+                ) {
+                    Row {
+                        Column {
+                            if (routeTypesErrorVisible) {
+                                Row {
+                                    ErrorText(text = stringResource(id = R.string.newroute_types_unselected_error))
+                                }
+                            }
+                            Row {
+                                PredefinedSpacer()
+                            }
+                        }
+                    }
+                    Row {
+                        when (recordingState) {
+                            RecordingState.READY -> StartRecordingButton {
+                                rutaNuevaViewModel.startRecording()
+                            }
+                            RecordingState.RECORDING -> RecordingButton(
+                                onClick = { rutaNuevaViewModel.stopRecording() },
+                                onWaypointAddRequest = { rutaNuevaViewModel.addWaypointRequest() }
+                            )
+                            RecordingState.RECORDED -> UploadButton {
+                                rutaNuevaViewModel.requestUpload()
+                            }
+                        }
                     }
                 }
             }
@@ -154,6 +209,48 @@ fun RutaNueva(
                     it.isNotBlank()
                 }
             )
+        }
+        
+        if (routeImageErrorPopupVisible) {
+            ErrorPopup(
+                message = stringResource(id = R.string.newroute_image_error),
+                buttonLabel = stringResource(id = R.string.global_popup_accept)
+            ) {
+                rutaNuevaViewModel.setRouteImageErrorVisible(false)
+            }
+        }
+
+        if (waypointBuilderVisible && rutaNuevaViewModel.canAddWaypoint()) {
+            WaypointPopup(
+                name = waypointName,
+                description = waypointDescription,
+                image = waypointImage,
+                onDismiss = { rutaNuevaViewModel.closeWaypointPopup() },
+                onChange = { name, description, image ->
+                       rutaNuevaViewModel.setWaypointData(name, description, image)
+                },
+                canSubmit = {name, description, image ->
+                    name.isNotBlank() && description.isNotBlank() && image != null
+                }
+            ) {
+                rutaNuevaViewModel.saveCurrentWaypoint()
+            }
+        } else if (waypointBuilderVisible) {
+            ErrorPopup(
+                message = stringResource(id = R.string.newroute_cannot_add_waypoint),
+                buttonLabel = stringResource(id = R.string.global_popup_accept)
+            ) {
+                rutaNuevaViewModel.closeWaypointPopup()
+            }
+        }
+
+        if (waypointCreatedPopupVisible) {
+            ErrorPopup(
+                message = stringResource(id = R.string.newroute_waypoint_added),
+                buttonLabel = stringResource(id = R.string.global_popup_accept)
+            ) {
+                rutaNuevaViewModel.setWaypointCreatedPopupVisible(false)
+            }
         }
     }
 }
@@ -262,7 +359,7 @@ fun Descripcion(
 
 @Composable
 fun Dificultad(
-    selected: RouteDifficulty,
+    selected: RouteDifficulty?,
     expanded: Boolean,
     onClick: () -> Unit = {},
     onDismiss: () -> Unit = {},
@@ -409,7 +506,7 @@ fun LabeledCheckbox(
     }
 }
 
-fun difficultyStringResource(difficulty: RouteDifficulty): Int {
+fun difficultyStringResource(difficulty: RouteDifficulty?): Int {
     return when (difficulty) {
         RouteDifficulty.TRIVIAL -> R.string.route_difficulty_trivial
         RouteDifficulty.EASY -> R.string.route_difficulty_easy
@@ -417,5 +514,256 @@ fun difficultyStringResource(difficulty: RouteDifficulty): Int {
         RouteDifficulty.HARD -> R.string.route_difficulty_hard
         RouteDifficulty.EXPERT -> R.string.route_difficulty_expert
         else -> R.string.route_difficulty_undefined
+    }
+}
+
+@Composable
+fun StartRecordingButton(
+    onClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                Color.White,
+                RoundedCornerShape(10.dp)
+            )
+            .clickable { onClick() },
+        contentAlignment = Alignment.Center
+    ) {
+        Row {
+            Column(Modifier.fillMaxHeight()) {
+                Box(
+                    modifier = Modifier.fillMaxHeight(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.newroute_button_start_recording),
+                        color = Color.Black,
+                        textAlign = TextAlign.Center,
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun RecordingButton(
+    onClick: () -> Unit,
+    onWaypointAddRequest: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(0.7f)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Color.Red,
+                        RoundedCornerShape(10.dp)
+                    )
+                    .clickable { onClick() },
+                contentAlignment = Alignment.Center
+            ) {
+                Row {
+                    Column(Modifier.fillMaxHeight()) {
+                        Box(
+                            modifier = Modifier.fillMaxHeight(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            CircularProgressIndicator(
+                                modifier = Modifier.padding(10.dp),
+                                color = Color.White
+                            )
+                        }
+                    }
+                    Column(Modifier.fillMaxHeight()) {
+                        Box(
+                            modifier = Modifier.fillMaxHeight(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(
+                                text = stringResource(id = R.string.newroute_button_recording),
+                                color = Color.Black,
+                                textAlign = TextAlign.Center,
+                            )
+                        }
+                    }
+                }
+            }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Color.Yellow,
+                        RoundedCornerShape(10.dp)
+                    )
+                    .clickable { onWaypointAddRequest() },
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = stringResource(id = R.string.newroute_add_waypoint_button),
+                    color = Color.Black,
+                    textAlign = TextAlign.Center,
+                    maxLines = 2
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun UploadButton(
+    onClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                Color.Yellow,
+                RoundedCornerShape(10.dp)
+            )
+            .clickable { onClick() },
+        contentAlignment = Alignment.Center
+    ) {
+        Row {
+            Column(Modifier.fillMaxHeight()) {
+                Box(
+                    modifier = Modifier.fillMaxHeight(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.newroute_button_upload),
+                        color = Color.Black,
+                        textAlign = TextAlign.Center,
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun WaypointPopup(
+    name: String,
+    description: String,
+    image: ImageBitmap?,
+
+    onDismiss: () -> Unit,
+    onChange: (name: String, description: String, image: ImageBitmap?) -> Unit,
+    canSubmit: (name: String, description: String, image: ImageBitmap?) -> Boolean,
+    onSuccess: () -> Unit
+) {
+    Dialog(
+        onDismissRequest = { onDismiss() },
+        properties = DialogProperties(
+            dismissOnBackPress = true,
+            dismissOnClickOutside = false
+        )
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp)
+                .background(MaterialTheme.colors.surface, shape = RoundedCornerShape(10.dp)),
+            contentAlignment = Alignment.Center
+        ) {
+            Column {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                        .clickable { CameraManager.takePicture { onChange(name, description, it) } }
+                ) {
+                    if (image != null) {
+                        Image(
+                            bitmap = image,
+                            contentDescription = stringResource(id = R.string.route_mainpicture_description)
+                                .format(image),
+                            modifier = Modifier.fillMaxSize(),
+                            contentScale = ContentScale.Crop
+                        )
+                    } else {
+                        Column(
+                            modifier = Modifier.fillMaxSize()
+                        ) {
+                            Row(
+                                Modifier
+                                    .fillMaxWidth()
+                                    .fillMaxHeight(0.6f)) {
+                                Box(
+                                    modifier = Modifier.fillMaxSize(),
+                                    contentAlignment = Alignment.BottomCenter
+                                ) {
+                                    Icon(
+                                        imageVector = Icons.Default.AddAPhoto,
+                                        contentDescription = stringResource(id = R.string.newroute_add_image),
+                                    )
+                                }
+                            }
+                            Row(Modifier.fillMaxSize()) {
+                                Box(
+                                    modifier = Modifier.fillMaxSize(),
+                                    contentAlignment = Alignment.TopCenter
+                                ){
+                                    Text(
+                                        text = stringResource(id = R.string.newroute_add_image),
+                                        textAlign = TextAlign.Center,
+                                        fontSize = 16.sp,
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+                Row(
+                    modifier = Modifier
+                        .padding(10.dp)
+                ) {
+                    TextField(
+                        value = name,
+                        onValueChange = { onChange(it, description, image) },
+                        label = { Text(text = stringResource(id = R.string.newroute_waypoint_name)) }
+                    )
+                }
+                Row(
+                    modifier = Modifier
+                        .padding(10.dp)
+                ) {
+                    TextField(
+                        value = description,
+                        onValueChange = { onChange(name, it, image) },
+                        label = { Text(text = stringResource(id = R.string.newroute_waypoint_description)) }
+                    )
+                }
+                Row {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Button(
+                            onClick = { if (canSubmit(name, description, image)) onSuccess() },
+                            enabled = canSubmit(name, description, image)
+                        ) {
+                            Text(text = stringResource(id = R.string.newroute_waypoint_submit_button))
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/intermodular/ui/feature/rutanueva/ui/RutaNuevaViewModel.kt
+++ app/src/main/java/com/example/intermodular/ui/feature/rutanueva/ui/RutaNuevaViewModel.kt
@@ -1,13 +1,26 @@
 package com.example.intermodular.ui.feature.rutanueva.ui
 
+import android.util.Log
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.NavHostController
+import com.example.intermodular.core.location.tracker.LocationRecorder
+import com.example.intermodular.core.route.ServerRouteManager
 import com.example.intermodular.core.route.model.RouteDifficulty
 import com.example.intermodular.core.route.model.RouteType
+import com.example.intermodular.model.Routes
+import com.example.intermodular.ui.feature.rutanueva.model.RawRoute
+import com.example.intermodular.ui.feature.rutanueva.model.RawWaypoint
+import kotlinx.coroutines.launch
 
-class RutaNuevaViewModel: ViewModel() {
+class RutaNuevaViewModel(val navigationController: NavHostController) : ViewModel() {
+
+    private val locationRecorder = LocationRecorder()
+    private val waypoints = mutableListOf<RawWaypoint>()
+
 
     // Fields
     private val _routeName = MutableLiveData<String>()
@@ -22,25 +35,51 @@ class RutaNuevaViewModel: ViewModel() {
     private val _routeTypes = MutableLiveData<List<RouteType>>()
     val routeTypes: LiveData<List<RouteType>> = _routeTypes
 
-    // Popup visibilities
 
+    // Popup visibilities
     private val _routeNamePopupVisible = MutableLiveData<Boolean>()
     val routeNamePopupVisible: LiveData<Boolean> = _routeNamePopupVisible
 
     private val _routeDifficultyPopupVisible = MutableLiveData<Boolean>()
     val routeDifficultyPopupVisible: LiveData<Boolean> = _routeDifficultyPopupVisible
 
-    // Error messages
-    private val _routeDescriptionErrorMessage = MutableLiveData<Int>()
-    val routeDescriptionErrorMessage: LiveData<Int> = _routeDescriptionErrorMessage
+    private val _waypointPopupVisible = MutableLiveData<Boolean>()
+    val waypointPopupVisible: LiveData<Boolean> = _waypointPopupVisible
+
+    private val _waypointCreatedPopupVisible = MutableLiveData<Boolean>()
+    val waypointCreatedPopupVisible: LiveData<Boolean> = _waypointCreatedPopupVisible
+
 
     // Error visibilities
     private val _routeDescriptionErrorVisible = MutableLiveData<Boolean>()
     val routeDescriptionErrorVisible: LiveData<Boolean> = _routeDescriptionErrorVisible
 
+    private val _routeTypesErrorVisible = MutableLiveData<Boolean>()
+    val routeTypesErrorVisible: LiveData<Boolean> = _routeTypesErrorVisible
+
+    private val _routeImageErrorVisible = MutableLiveData<Boolean>()
+    val routeImageErrorVisible: LiveData<Boolean> = _routeImageErrorVisible
+
+    private val _routeDifficultyErrorVisible = MutableLiveData<Boolean>()
+    val routeDifficultyErrorVisible: LiveData<Boolean> = _routeDifficultyErrorVisible
+
+
     // Other
     private val _mainImageBitmap = MutableLiveData<ImageBitmap?>()
     val mainImageBitmap: LiveData<ImageBitmap?> = _mainImageBitmap
+
+    private val _recordingState = MutableLiveData<RecordingState>()
+    val recordingState: LiveData<RecordingState> = _recordingState
+
+    private val _waypointName = MutableLiveData<String>()
+    val waypointName: LiveData<String> = _waypointName
+
+    private val _waypointDescription = MutableLiveData<String>()
+    val waypointDescription: LiveData<String> = _waypointDescription
+
+    private val _waypointImage = MutableLiveData<ImageBitmap>()
+    val waypointImage: LiveData<ImageBitmap> = _waypointImage
+
 
     fun setName(name: String) {
         _routeName.value = name
@@ -73,4 +112,140 @@ class RutaNuevaViewModel: ViewModel() {
     fun setRouteDifficultyPopupVisible(visible: Boolean) {
         _routeDifficultyPopupVisible.value = visible
     }
+
+    fun setRouteImageErrorVisible(visible: Boolean) {
+        _routeImageErrorVisible.value = visible
+    }
+
+    fun setWaypointCreatedPopupVisible(visible: Boolean) {
+        _waypointCreatedPopupVisible.value = visible
+    }
+
+    fun canAddWaypoint(): Boolean {
+        return _recordingState.value == RecordingState.RECORDING && locationRecorder.lastMark() != null
+    }
+
+    fun setWaypointData(
+        name: String,
+        description: String,
+        image: ImageBitmap?
+    ) {
+        _waypointName.value = name
+        _waypointDescription.value = description
+        _waypointImage.value = image
+    }
+
+    fun saveCurrentWaypoint() {
+
+        waypoints.add(
+            RawWaypoint(
+                _waypointName.value ?: "",
+                _waypointDescription.value ?: "",
+                locationRecorder.lastMark()!!.latitude,
+                locationRecorder.lastMark()!!.longitude,
+                _waypointImage.value ?: ImageBitmap(1, 1)
+            )
+        )
+        setWaypointCreatedPopupVisible(true)
+        closeWaypointPopup()
+    }
+
+    fun closeWaypointPopup() {
+        _waypointPopupVisible.value = false
+        resetWaypointData()
+    }
+
+    fun startRecording() {
+        _recordingState.value = RecordingState.RECORDING
+        locationRecorder.startRecording()
+    }
+
+    fun stopRecording() {
+        locationRecorder.stopRecording()
+
+        if (locationRecorder.currentMarks().isEmpty()) {
+            _recordingState.value = RecordingState.READY
+        } else {
+            _recordingState.value = RecordingState.RECORDED
+        }
+    }
+
+    fun addWaypointRequest() {
+        resetWaypointData()
+        _waypointPopupVisible.value = true
+    }
+
+    private fun resetWaypointData() {
+        _waypointName.value = ""
+        _waypointDescription.value = ""
+        _waypointImage.value = null
+    }
+
+    fun requestUpload() {
+        if (!checkFields()) {
+            showErrors()
+            return
+        }
+
+        val route = RawRoute(
+            _routeName.value!!,
+            _routeDescription.value!!,
+            _routeDifficulty.value!!,
+            _routeTypes.value!!,
+            _mainImageBitmap.value!!,
+            locationRecorder.currentMarks(),
+            waypoints
+        )
+
+        viewModelScope.launch {
+            val result = ServerRouteManager.uploadRoute(route.asUploadableRoute())
+            if (result) {
+                Log.i("RouteUpload", "Route uploaded successfully")
+                navigationController.navigate(Routes.HomeScreen.route)
+            } else {
+                Log.i("RouteUpload", "Route upload failed")
+                navigationController.navigate(Routes.UserInfoScreen.route)
+            }
+        }
+    }
+
+    private fun checkFields(): Boolean =
+                checkName()
+                && checkDescription()
+                && checkDifficulty()
+                && checkTypes()
+                && checkImage()
+                && checkRecording()
+
+    private fun checkName(): Boolean        = _routeName.value?.isNotEmpty() ?: false
+
+    private fun checkDescription(): Boolean = _routeDescription.value?.isNotEmpty() ?: false
+
+    private fun checkDifficulty(): Boolean  = _routeDifficulty.value != null
+
+    private fun checkTypes(): Boolean       = _routeTypes.value?.isNotEmpty() ?: false
+
+    private fun checkImage(): Boolean       = _mainImageBitmap.value != null
+
+    private fun checkRecording(): Boolean   = _recordingState.value == RecordingState.RECORDED && locationRecorder.currentMarks().isNotEmpty()
+
+    private fun showErrors() {
+        if (!checkName()) {
+            _routeNamePopupVisible.value = true
+            return
+        }
+
+        _routeDescriptionErrorVisible.value     = !checkDescription()
+        _routeDifficultyErrorVisible.value      = !checkDifficulty()
+        _routeTypesErrorVisible.value           = !checkTypes()
+        _routeImageErrorVisible.value           = !checkImage()
+
+    }
+
+}
+
+enum class RecordingState {
+    READY,
+    RECORDING,
+    RECORDED
 }
