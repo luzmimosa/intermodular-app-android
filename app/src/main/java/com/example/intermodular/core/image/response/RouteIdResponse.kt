94596df0263cafa8066099e043a0b6df783db152
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-02-18 04:40:00 +0100

    respuesta para un id de ruta

---
+++ app/src/main/java/com/example/intermodular/core/image/response/RouteIdResponse.kt
@@ -0,0 +1,7 @@
+package com.example.intermodular.core.image.response
+
+import com.google.gson.annotations.SerializedName
+
+data class RouteIdResponse(
+    @SerializedName("id")   val id: String
+)
