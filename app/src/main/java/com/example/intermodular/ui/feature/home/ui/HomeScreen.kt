f716adee7f7a3825ea2ea7736445a7b4c9482d13
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-02-20 15:54:25 +0100

    la cola de rutas se consulta ahora cada segundo

--- app/src/main/java/com/example/intermodular/ui/feature/home/ui/HomeScreen.kt
+++ app/src/main/java/com/example/intermodular/ui/feature/home/ui/HomeScreen.kt
@@ -5,9 +5,12 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -15,13 +18,22 @@ import androidx.navigation.NavHostController
 import com.example.intermodular.core.route.model.Route
 import com.example.intermodular.ui.component.global.*
 import com.example.intermodular.ui.component.route.RouteCard
+import kotlinx.coroutines.delay
 
 @Composable
 fun Home(homeviewmodel : HomeViewModel, navigationController: NavHostController){
 
     val routes: Array<Route> by homeviewmodel.routes.observeAsState(initial = arrayOf())
 
-    homeviewmodel.refreshRoutes()
+    LaunchedEffect(true) {
+        homeviewmodel.fetchRoutes()
+    }
+    LaunchedEffect(true) {
+        while (true) {
+            delay(1000)
+            homeviewmodel.refreshRoutes()
+        }
+    }
 
     WikihonkBaseScreen(navigationController = navigationController) {
         RoutesContainer(
@@ -38,7 +50,6 @@ fun RoutesContainer(
     onLoadMore: () -> Unit
 ) {
     val lazyListState = rememberLazyListState()
-
     LazyColumn(
         state = lazyListState,
         contentPadding = PaddingValues(horizontal = 2.dp, vertical = 0.dp),
@@ -48,14 +59,27 @@ fun RoutesContainer(
             .fillMaxSize()
     ) {
         items(items = routes) { route ->
-            Row(){
+            Row {
                 RouteCard(route = route)
             }
         }
 
+        item {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(64.dp)
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+
+
         if (lazyListState.layoutInfo.visibleItemsInfo.lastOrNull()?.index == routes.lastIndex) {
             onLoadMore()
         }
+
     }
 }
 
