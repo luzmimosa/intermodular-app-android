a78590f871f5c9803fc0304ae59f19eb6d0d9980
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-02-20 15:55:29 +0100

    asegurada la petición de rutas con estructuras try catch

--- app/src/main/java/com/example/intermodular/core/route/ServerRouteManager.kt
+++ app/src/main/java/com/example/intermodular/core/route/ServerRouteManager.kt
@@ -1,5 +1,6 @@
 package com.example.intermodular.core.route
 
+import android.util.Log
 import com.example.intermodular.core.network.RetrofitHelper
 import com.example.intermodular.core.route.client.NearRoutesClient
 import com.example.intermodular.core.route.client.RandomRoutesClient
@@ -7,6 +8,7 @@ import com.example.intermodular.core.route.client.RouteClient
 import com.example.intermodular.core.route.client.RouteUploadClient
 import com.example.intermodular.core.route.model.*
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.time.LocalDateTime
 import java.time.ZoneOffset
@@ -17,93 +19,130 @@ object ServerRouteManager {
 
     suspend fun getRouteByID(routeID: String): Route? {
         return withContext(Dispatchers.IO) {
-            val response = retrofit.create(RouteClient::class.java).getRoute(routeID)
+            try {
+                val response = retrofit.create(RouteClient::class.java).getRoute(routeID)
 
-            if (!response.isSuccessful) return@withContext null
+                if (!response.isSuccessful) return@withContext null
 
-            val routeResponse = response.body() ?: return@withContext null
+                val routeResponse = response.body() ?: return@withContext null
 
-            val locations: Array<ServerGpsMeasure> = routeResponse.measures.map { measure ->
-                val waypoint: ServerWaypoint? = if (measure.waypoint == null) null else ServerWaypoint(measure.waypoint.name, measure.waypoint.description, measure.waypoint.imagePath)
-                return@map ServerGpsMeasure(measure.latitude, measure.longitude, waypoint)
-            }.toTypedArray()
-            val types: Array<RouteType> = if (routeResponse.types == null) arrayOf() else routeResponse.types.map { type ->
-                try {
-                    return@map RouteType.valueOf(type)
+                val locations: Array<ServerGpsMeasure> = routeResponse.measures.map { measure ->
+                    val waypoint: ServerWaypoint? =
+                        if (measure.waypoint == null) null else ServerWaypoint(
+                            measure.waypoint.name,
+                            measure.waypoint.description,
+                            measure.waypoint.imagePath
+                        )
+                    return@map ServerGpsMeasure(measure.latitude, measure.longitude, waypoint)
+                }.toTypedArray()
+                val types: Array<RouteType> =
+                    if (routeResponse.types == null) arrayOf() else routeResponse.types.map { type ->
+                        try {
+                            return@map RouteType.valueOf(type)
+                        } catch (exception: java.lang.IllegalArgumentException) {
+                            return@map null
+                        }
+                    }.filterNotNull().toTypedArray()
+
+                val difficulty: RouteDifficulty = try {
+                    RouteDifficulty.valueOf(routeResponse.difficulty)
                 } catch (exception: java.lang.IllegalArgumentException) {
-                    return@map null
+                    RouteDifficulty.MEDIUM
                 }
-            }.filterNotNull().toTypedArray()
 
-            val difficulty: RouteDifficulty = try {
-                RouteDifficulty.valueOf(routeResponse.difficulty)
-            } catch (exception: java.lang.IllegalArgumentException) {
-                RouteDifficulty.MEDIUM
+                try {
+                    return@withContext ServerRoute(
+                        uid = routeResponse.uid,
+                        name = routeResponse.name,
+                        description = routeResponse.description,
+                        imageID = routeResponse.imagePath,
+                        lengthInKm = routeResponse.length,
+                        locations = locations,
+                        types = types,
+                        difficulty = difficulty,
+                        creator = routeResponse.creator,
+                        creationDatetime = LocalDateTime.ofEpochSecond(
+                            routeResponse.creationDatetime / 1000,
+                            0,
+                            ZoneOffset.UTC
+                        ),
+                        likes = routeResponse.likes
+                    ).asRoute()
+                } catch (exception: Exception) {
+                    Log.e("ServerRouteManager", "Error while resolving route by ID", exception)
+                    return@withContext null
+                }
+            } catch (exception: Exception) {
+                Log.e("ServerRouteManager", "Error while getting route by ID", exception)
+                return@withContext null
             }
-
-
-            return@withContext ServerRoute(
-                uid = routeResponse.uid,
-                name = routeResponse.name,
-                description = routeResponse.description,
-                imageID = routeResponse.imagePath,
-                lengthInKm = routeResponse.length,
-                locations = locations,
-                types = types,
-                difficulty = difficulty,
-                creator = routeResponse.creator,
-                creationDatetime = LocalDateTime.ofEpochSecond(routeResponse.creationDatetime / 1000, 0, ZoneOffset.UTC),
-                likes = routeResponse.likes
-            ).asRoute()
         }
     }
 
-    suspend fun getRoutesByLocation(latitude: Double, longitude: Double, radius: Int = 30, limit: Int = 20): Array<Route> {
-        try {
-            return withContext(Dispatchers.IO) {
+    suspend fun provideRoutesByLocation(
+        latitude: Double,
+        longitude: Double,
+        radius: Int = 30,
+        provide: (route: Route) -> Unit
+    ) {
 
-                val response = retrofit.create(NearRoutesClient::class.java).getNearRoutes(latitude, longitude, radius)
-                if (!response.isSuccessful) return@withContext arrayOf()
+        withContext(Dispatchers.IO) {
+            this.launch {
+                try {
+                    val routeIdsResponse = retrofit.create(NearRoutesClient::class.java)
+                        .getNearRoutes(latitude, longitude, radius)
+                    if (!routeIdsResponse.isSuccessful) return@launch
+                    val routeIds = routeIdsResponse.body() ?: return@launch
 
-                val routeIdsResponse = response.body() ?: return@withContext arrayOf()
-
-                val routes = mutableListOf<Route>()
-
-                for (routeID in routeIdsResponse) {
-                    val route = getRouteByID(routeID)
-                    if (route != null && routes.size < limit) {
-                        routes.add(route)
+                    for (routeID in routeIds) {
+                        this.launch {
+                            try {
+                                val route = getRouteByID(routeID)
+                                if (route != null) {
+                                    provide(route)
+                                }
+                            } catch (exception: Exception) {
+                                Log.e("ServerRouteManager", "Error while resolving route by location", exception)
+                            }
+                        }
                     }
+                } catch (exception: Exception) {
+                    Log.e("ServerRouteManager", "Error while getting routes by location", exception)
+                    return@launch
                 }
-
-                return@withContext routes.toTypedArray()
             }
-        } catch (exception: Exception) {
-            return arrayOf()
         }
     }
 
-    suspend fun getRandomRoutes(max: Int = 10): Array<Route> {
-        try {
-            return withContext(Dispatchers.IO) {
-                val response = retrofit.create(RandomRoutesClient::class.java).getRandomRoutes()
-                if (!response.isSuccessful) return@withContext arrayOf()
+    suspend fun provideRandomRoutes(
+        provide: (route: Route) -> Unit
+    ){
 
-                val routeIdsResponse = response.body() ?: return@withContext arrayOf()
+        withContext(Dispatchers.IO) {
+            this.launch {
+                try {
+                    val response = retrofit.create(RandomRoutesClient::class.java).getRandomRoutes()
+                    if (!response.isSuccessful) return@launch
 
-                val routes = mutableListOf<Route>()
+                    val routeIdsResponse = response.body() ?: return@launch
 
-                for (routeID in routeIdsResponse) {
-                    val route = getRouteByID(routeID)
-                    if (route != null && routes.size < max) {
-                        routes.add(route)
+                    for (routeID in routeIdsResponse) {
+                        this.launch {
+                            try {
+                                val route = getRouteByID(routeID)
+                                if (route != null) {
+                                    provide(route)
+                                }
+                            } catch (exception: Exception) {
+                                Log.e("ServerRouteManager", "Error while resolving random route", exception)
+                            }
+                        }
                     }
+                } catch (exception: Exception) {
+                    Log.e("ServerRouteManager", "Error while getting random routes", exception)
+                    return@launch
                 }
-
-                return@withContext routes.toTypedArray()
             }
-        } catch (exception: Exception) {
-            return arrayOf()
         }
     }
 
