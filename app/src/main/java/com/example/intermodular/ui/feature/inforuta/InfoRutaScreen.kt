63e3c8195e7b6588dab3d7ab9eaaa1afa90495ec
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-02-21 10:17:11 +0100

    avanzada la implementación de la ventana de info de ruta

--- app/src/main/java/com/example/intermodular/ui/feature/inforuta/InfoRutaScreen.kt
+++ app/src/main/java/com/example/intermodular/ui/feature/inforuta/InfoRutaScreen.kt
@@ -1,111 +1,81 @@
 package com.example.intermodular.ui.feature.inforuta
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.intermodular.R
+import com.example.intermodular.core.route.model.Route
+import com.example.intermodular.core.route.model.RouteDifficulty
+import com.example.intermodular.core.route.model.RouteType
 import com.example.intermodular.model.Routes
 import com.example.intermodular.ui.component.global.*
+import com.example.intermodular.ui.component.route.routeTypeIcon
+import kotlinx.coroutines.delay
+import java.time.LocalDateTime
 
-@Preview
 @Composable
-fun InfoRuta(infoRutaViewModel: InfoRutaViewModel= InfoRutaViewModel(), navigationController: NavHostController= rememberNavController()){
-    Scaffold(
-        topBar= {
-            WikihonkTopBar(navigationController = navigationController)
-        },
+fun InfoRuta(
+    infoRutaViewModel: InfoRutaViewModel,
+    navigationController: NavHostController
+){
 
-        bottomBar= {
-            WikihonkBottomBar(navigationController)
+    val route: Route by infoRutaViewModel.route.observeAsState(initial = Route(
+        uid = "0",
+        name = "...",
+        description = "...",
+        image = ImageBitmap(1, 1),
+        lengthInKm = 0.0,
+        locations = arrayOf(),
+        types = arrayOf(RouteType.BYCICLE, RouteType.WALK, RouteType.RUNNING),
+        difficulty = RouteDifficulty.MEDIUM,
+        creator = "...",
+        creationDatetime = LocalDateTime.now(),
+        likes = 0
+    ))
+
+    val isResolved by infoRutaViewModel.isRouteResolved.observeAsState(initial = false)
+
+    LaunchedEffect(true) {
+        infoRutaViewModel.fetchRoute()
+        while (!isResolved) {
+            infoRutaViewModel.resolveRoute()
+            delay(1000)
         }
+    }
+
+    WikihonkBaseScreen(
+        navigationController = navigationController
     ) {
         Column(modifier= Modifier
             .fillMaxSize()
-            .background(color = Color.Gray)){
+            .background(color = Color.Gray)
+        ){
+
+            RouteHeader(route, navigationController)
+
             Row(modifier= Modifier
-                .padding(10.dp, 0.dp)
                 .fillMaxWidth()
-                .paint(
-                    painterResource(id = R.drawable.black)
-                    //tiene que ser de esa altura(height) pero se tiene que rellenar de los lados
-                )){
-                
-            }
-            
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .padding(5.dp, 0.dp)
+                .padding(5.dp)
                 .background(color = Color.White)){
-
-                //redondear bordes SOLO LOS DE ABAJO
-
-                Box(modifier = Modifier.padding(10.dp, 20.dp, 10.dp, 10.dp)){
-                    ProfilePicture(username = "wpf")
-                }
-
-                Box(modifier= Modifier.padding(10.dp)){
-                    Text(
-                        text = "Nombre de la ruta",
-                        fontSize = 20.sp,
-                        modifier = Modifier.padding(10.dp, 5.dp, 0.dp, 0.dp)
-                    )
-
-                    Text(
-                        text = "Nombre de usuario",
-                        fontSize = 15.sp,
-                        modifier= Modifier.padding(10.dp, 35.dp, 0.dp, 0.dp)
-                    )
-
-                    Text(
-                        text = "Dificultad: ",
-                        fontSize = 15.sp,
-                        modifier= Modifier.padding(10.dp, 55.dp, 0.dp, 0.dp)
-                    )
-                    Text(
-                        text = "X",
-                        fontSize = 15.sp,
-                        modifier= Modifier.padding(10.dp, 75.dp, 0.dp, 0.dp)
-                    )
-
-                    Box(modifier = Modifier.padding(10.dp, 100.dp, 0.dp, 0.dp)){
-                        ClickableText(text = "Ver ruta", fontSize = 20.sp, fontWeight = FontWeight.Bold, underlined = true) {
-                            navigationController.navigate(Routes.MapScreen.route)
-                        }
-                    }
-                }
-
-                Box(modifier= Modifier.padding(5.dp, 95.dp, 10.dp, 5.dp)){
-                    IconButton(
-                        onClick = { /*marcar en favoritos*/ },
-                    ) {
-                       Icon(
-                           imageVector= Icons.Filled.Favorite,
-                           contentDescription= stringResource(id = R.string.info_ruta_favorite),
-                           tint= Color.Red,
-                           modifier = Modifier.size(50.dp)
-                       )
-                    }
-                }
-            }
-
-            Row(modifier= Modifier.fillMaxWidth().padding(5.dp).background(color = Color.White)){
                 //redondear bordes
                 Text(
                     text = "COMENTARIOS",
@@ -115,12 +85,15 @@ fun InfoRuta(infoRutaViewModel: InfoRutaViewModel= InfoRutaViewModel(), navigati
                 )
             }
 
-            Row(modifier= Modifier.fillMaxWidth().padding(5.dp).background(color = Color.White)){
+            Row(modifier= Modifier
+                .fillMaxWidth()
+                .padding(5.dp)
+                .background(color = Color.White)){
                 //redondear bordes
                 //poner imagen mas grande
                 //añadir lazy column para la tarjeta de comentarios
 
-                Box(){
+                Box {
                     SmallProfilePicture(username = "wpf")
                 }
 
@@ -142,3 +115,175 @@ fun InfoRuta(infoRutaViewModel: InfoRutaViewModel= InfoRutaViewModel(), navigati
     }
 }
 
+@Composable
+fun RouteHeader(
+    route: Route,
+    navigationController: NavHostController = rememberNavController(),
+    isFavorite: Boolean = false,
+    isToDo: Boolean = false,
+    onFavPress: () -> Unit = {},
+    onToDoPress: () -> Unit = {}
+) {
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = MaterialTheme.colors.surface)
+    ) {
+        Column {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp)
+            ) {
+                Image(
+                    bitmap = route.image,
+                    contentDescription = stringResource(id = R.string.home_alt_route_image).format(
+                        route.name
+                    ),
+                    contentScale = ContentScale.Crop
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp)
+                    .padding(5.dp, 0.dp)
+            ) {
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .padding(0.dp, 0.dp, 10.dp, 0.dp)
+                ) {
+                    Box(modifier = Modifier.fillMaxHeight(), contentAlignment = Alignment.Center) {
+                        ProfilePicture(username = route.creator)
+                    }
+                }
+                Column {
+                    Row {
+                        Text(
+                            text = route.name,
+                            fontSize = 20.sp
+                        )
+                    }
+
+                    Row {
+                        Text(
+                            text = "@${route.creator}",
+                            fontSize = 15.sp
+                        )
+                    }
+
+                    Row {
+                        Text(
+                            text = when (route.difficulty) {
+                                RouteDifficulty.TRIVIAL -> stringResource(id = R.string.route_difficulty_trivial)
+                                RouteDifficulty.EASY -> stringResource(id = R.string.route_difficulty_easy)
+                                RouteDifficulty.MEDIUM -> stringResource(id = R.string.route_difficulty_medium)
+                                RouteDifficulty.HARD -> stringResource(id = R.string.route_difficulty_hard)
+                                RouteDifficulty.EXPERT -> stringResource(id = R.string.route_difficulty_expert)
+                            },
+                            fontSize = 15.sp
+                        )
+                    }
+                    Row {
+                        for (type in route.types) {
+                            Column {
+                                Icon(
+                                    imageVector = routeTypeIcon(type),
+                                    contentDescription = "Type",
+                                )
+                            }
+                        }
+                    }
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxHeight(0.9f),
+                            contentAlignment = Alignment.BottomCenter
+                        ) {
+                            ClickableText(
+                                text = "Ver ruta",
+                                fontSize = 18.sp,
+                                fontWeight = FontWeight.Bold,
+                                underlined = true
+                            ) {
+                                navigationController.navigate(Routes.MapScreen.route)
+                            }
+                        }
+                    }
+                }
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight(0.5f)
+                    ) {
+                        Box(
+                            contentAlignment = Alignment.CenterEnd,
+                            modifier = Modifier
+                                .fillMaxSize()
+                        ) {
+                            FavButton(isFavorite) {
+                                onFavPress()
+                            }
+                        }
+                    }
+                    Row(
+                        modifier = Modifier
+                            .fillMaxSize()
+                    ) {
+                        Box(
+                            contentAlignment = Alignment.CenterEnd,
+                            modifier = Modifier
+                                .fillMaxSize()
+                        ) {
+                            ToDoButton(isToDo) {
+                                onToDoPress()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun FavButton(
+    active: Boolean,
+    onPress: () -> Unit
+) {
+    Icon(
+        imageVector = if (active) Icons.Filled.Favorite else Icons.Filled.FavoriteBorder,
+        contentDescription = "Fav",
+        modifier = Modifier
+            .clickable { onPress() }
+            .padding(10.dp),
+        tint = if (active) Color.Red else Color.Gray
+    )
+}
+
+@Composable
+fun ToDoButton(
+    active: Boolean,
+    onPress: () -> Unit
+) {
+    Icon(
+        imageVector = if (active) Icons.Filled.TaskAlt else Icons.Filled.AddTask,
+        contentDescription = "To do",
+        modifier = Modifier
+            .clickable { onPress() }
+            .padding(10.dp),
+        tint = if (active) Color.Green else Color.Gray
+    )
+}
--- app/src/main/java/com/example/intermodular/ui/feature/inforuta/InfoRutaViewModel.kt
+++ app/src/main/java/com/example/intermodular/ui/feature/inforuta/InfoRutaViewModel.kt
@@ -1,4 +1,39 @@
 package com.example.intermodular.ui.feature.inforuta
 
-class InfoRutaViewModel {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.intermodular.core.route.ServerRouteManager
+import com.example.intermodular.core.route.model.Route
+
+class InfoRutaViewModel(val routeID: String): ViewModel() {
+
+    private val _route = MutableLiveData<Route>()
+    val route: LiveData<Route> = _route
+
+    private val _isRouteResolved = MutableLiveData<Boolean>()
+    val isRouteResolved: LiveData<Boolean> = _isRouteResolved
+
+    private var resolvedRoute: Route? = null
+
+    suspend fun fetchRoute() {
+        val fetchedRoute = ServerRouteManager.getRouteByID(routeID)
+
+        if (fetchedRoute != null) {
+            resolvedRoute = fetchedRoute
+        } else {
+            Log.i("InfoRutaViewModel", "Error fetching route")
+        }
+    }
+
+    fun resolveRoute() {
+        if (resolvedRoute != null) {
+            _route.value = resolvedRoute
+            _isRouteResolved.value = true
+        } else {
+            Log.i("InfoRutaViewModel", "Error resolving route")
+        }
+    }
+
 }
