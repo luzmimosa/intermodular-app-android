827f501ff666cd8f9f6d953535df581975c56a05
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-02-22 21:30:48 +0100

    cliente para obtener usuarios

---
+++ app/src/main/java/com/example/intermodular/core/user/client/UserClient.kt
@@ -0,0 +1,16 @@
+package com.example.intermodular.core.user.client
+
+import com.example.intermodular.core.user.model.ServerUser
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface UserClient {
+
+    @GET("/api/v1/user/{username}")
+    suspend fun getUserByUsername(@Path("username")username: String): Response<ServerUser>
+
+    @GET("/api/v1/user")
+    suspend fun getSelfUser(): Response<ServerUser>
+
+}
