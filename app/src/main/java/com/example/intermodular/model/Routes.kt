ec5845c35f409f22756c9ba0f5f4718ed18e47cc
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-02-23 16:28:15 +0100

    la ruta mapa ahora acepta una ruta concreta

--- app/src/main/java/com/example/intermodular/model/Routes.kt
+++ app/src/main/java/com/example/intermodular/model/Routes.kt
@@ -6,8 +6,13 @@ sealed class Routes (val route: String){
     object RegisterScreen: Routes("registro")
     object UserInfoScreen: Routes("userinfo")
     object FavScreen: Routes("favoritos")
-    object MapScreen: Routes("mapa")
     object RutaNuevaScreen: Routes("rutanueva")
+
+    object MapScreen: Routes("mapa/{focusedRouteID}") {
+        fun route(focusedRouteID: String? = null): String {
+            return "mapa/${focusedRouteID ?: "null"}"
+        }
+    }
     object InfoRuta: Routes("inforuta/{routeID}") {
         fun route(routeID: String): String {
             return "inforuta/$routeID"
