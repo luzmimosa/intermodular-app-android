65bee802db65d51479b88f5ed806bc1029ed73d0
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-02-23 16:28:51 +0100

    ahora el mapa puede cargar una ruta o las rutas almacenadas en caché

--- app/src/main/java/com/example/intermodular/ui/feature/map/ui/MapScreen.kt
+++ app/src/main/java/com/example/intermodular/ui/feature/map/ui/MapScreen.kt
@@ -14,9 +14,10 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.intermodular.R
-import com.example.intermodular.core.route.model.Route
+import com.example.intermodular.model.Routes
 import com.example.intermodular.ui.component.global.WikihonkBottomBar
 import com.example.intermodular.ui.component.global.WikihonkTopBar
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
@@ -27,13 +28,8 @@ fun MapScreen(
     mapViewModel: MapViewModel,
     navigationController: NavHostController
 ){
-    val camLocation: LatLng by mapViewModel.mapLocation.observeAsState(LatLng(0.0, 0.0))
-
-    val cameraPositionState= rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(camLocation, 13f)
-    }
-
-    val routes: List<Route> by mapViewModel.routes.observeAsState(listOf())
+    val cameraPositionState: CameraPositionState by mapViewModel.cameraPosition.observeAsState(CameraPositionState(CameraPosition.fromLatLngZoom(LatLng(0.0, 0.0), 0f)))
+    val routes by mapViewModel.routes.observeAsState(emptyArray())
 
     mapViewModel.setupMap()
 
@@ -63,11 +59,27 @@ fun MapScreen(
                 uiSettings = MapUiSettings(zoomControlsEnabled = false)
             ) {
                 for (route in routes) {
+                    Marker(
+                        state = MarkerState(
+                            position = LatLng(route.locations.first().latitude, route.locations.first().longitude),
+                        ),
+                        title = route.name,
+                        snippet = route.description,
+                        icon = BitmapDescriptorFactory.defaultMarker(
+                            rgbToHue(routeIdToColor(route.uid))
+                        ),
+                        onClick = {
+                            navigationController.navigate(Routes.InfoRuta.route(route.uid))
+                            false
+                        }
+                    )
                     Polyline(
                         points = route.locations.map { LatLng(it.latitude, it.longitude) },
                         color = routeIdToColor(route.uid),
-                        width = 5f
-                    )
+                        width = 8f
+                    ) {
+                        navigationController.navigate(Routes.InfoRuta.route(route.uid))
+                    }
                 }
             }
         }
@@ -81,4 +93,21 @@ fun routeIdToColor(routeId: String): Color {
     val g = (hash and 0x00FF00) shr 8
     val b = hash and 0x0000FF
     return Color(r, g, b)
+}
+
+fun rgbToHue(color: Color): Float {
+    val r = color.red
+    val g = color.green
+    val b = color.blue
+    val max = maxOf(r, g, b)
+    val min = minOf(r, g, b)
+    val delta = max - min
+    val hue = when {
+        delta == 0.0f -> 0f
+        max == r -> ((g - b) / delta) % 6
+        max == g -> ((b - r) / delta) + 2
+        else -> ((r - g) / delta) + 4
+    }
+
+    return Math.abs((hue * 60) % 360)
 }
--- app/src/main/java/com/example/intermodular/ui/feature/map/ui/MapViewModel.kt
+++ app/src/main/java/com/example/intermodular/ui/feature/map/ui/MapViewModel.kt
@@ -4,10 +4,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.intermodular.core.location.WikihonkLocationManager
 import com.example.intermodular.core.route.ServerRouteManager
 import com.example.intermodular.core.route.model.Route
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.CameraPositionState
 import kotlinx.coroutines.launch
 
 
@@ -15,65 +16,37 @@ class MapViewModel(
     val routeID: String?
 ): ViewModel() {
 
-    private val _mapLocation = MutableLiveData<LatLng>()
-    val mapLocation: LiveData<LatLng> = _mapLocation
+    private val _cameraPosition = MutableLiveData<CameraPositionState>()
+    val cameraPosition: LiveData<CameraPositionState> = _cameraPosition
 
-    private val _routes = MutableLiveData<List<Route>>()
-    val routes: LiveData<List<Route>> = _routes
-
-    private val routeQueue = mutableListOf<Route>()
+    private val _routes = MutableLiveData<Array<Route>>(arrayOf())
+    val routes: LiveData<Array<Route>> = _routes
 
     fun setupMap() {
-        _mapLocation.value = LatLng(
-            WikihonkLocationManager.userLocation.value?.latitude ?: 0.0,
-            WikihonkLocationManager.userLocation.value?.longitude ?: 0.0
-        )
-
-        viewModelScope.launch {
-            loadRoutes()
-        }
-    }
-
-    private suspend fun loadRoutes() {
-        if (routeID != null) {
-            loadSpecificRoute()
+        if (routeID != null && routeID != "null") {
+            loadSingleRoute()
         } else {
-            loadNearbyRoutes()
-            loadRandomRoutes()
+            loadAllRoutes()
         }
     }
 
-    private suspend fun loadSpecificRoute() {
-        if (routeID != null) {
-            val route = ServerRouteManager.getRouteByID(routeID) ?: return
-
-            _routes.value = listOf(route)
-            _mapLocation.value = LatLng(
-                route.locations.first().latitude,
-                route.locations.first().longitude
+    private fun loadSingleRoute() {
+        viewModelScope.launch {
+            val route = ServerRouteManager.getRouteByID(routeID!!) ?: return@launch
+            _routes.value = arrayOf(route)
+            _cameraPosition.value = CameraPositionState(
+                CameraPosition.fromLatLngZoom(
+                    LatLng(
+                        route.locations.first().latitude,
+                        route.locations.first().longitude
+                    ),
+                    15f
+                )
             )
-
         }
     }
 
-    private suspend fun loadNearbyRoutes() {
-        ServerRouteManager.provideRoutesByLocation(
-            WikihonkLocationManager.userLocation.value?.latitude ?: 0.0,
-            WikihonkLocationManager.userLocation.value?.longitude ?: 0.0,
-        ) {
-            routeQueue.add(it)
-            loadRoutesFromQueue()
-        }
-    }
-
-    private suspend fun loadRandomRoutes() {
-        ServerRouteManager.provideRandomRoutes {
-            routeQueue.add(it)
-            loadRoutesFromQueue()
-        }
-    }
-
-    private fun loadRoutesFromQueue() {
-        _routes.value = _routes.value?.plus(routeQueue) ?: routeQueue
+    private fun loadAllRoutes() {
+        _routes.value = ServerRouteManager.getRouteCache().toTypedArray()
     }
 }
