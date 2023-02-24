005f61297c3de70963ff06164c8abf6f1afbf928
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-02-24 20:33:13 +0100

    rehecha la vista de rutas para soportar filtros de rutas. Optimizada la carga de rutas

--- app/src/main/java/com/example/intermodular/ui/feature/home/ui/HomeViewModel.kt
+++ app/src/main/java/com/example/intermodular/ui/feature/home/ui/HomeViewModel.kt
@@ -3,50 +3,143 @@ package com.example.intermodular.ui.feature.home.ui
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.intermodular.core.location.WikihonkLocationManager
 import com.example.intermodular.core.route.ServerRouteManager
 import com.example.intermodular.core.route.model.Route
+import com.example.intermodular.core.user.ServerUserManager
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel(
+    routeClassification: RouteClassification,
+) : ViewModel() {
 
-    val _routes = MutableLiveData<Array<Route>>()
-    val routes: LiveData<Array<Route>> = _routes
+    private var _routeClassification = routeClassification
+    val routeClassification: RouteClassification
+        get() = _routeClassification
 
-    private val routeQueue = mutableListOf<Route>()
+    val routes = MutableLiveData<Array<Route>>()
 
-    fun refreshRoutes() {
-        checkRouteQueue()
+    val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    var loadingScore = 1
+
+    fun requestRoutes() {
+        viewModelScope.launch {
+            cacheAllRoutes()
+            loadingScore -= 1
+
+            while (loadingScore > 0) {
+                delay(500)
+            }
+
+            filterRoutesFromCache()
+
+        }
     }
 
-    suspend fun fetchRoutes() {
+    fun filterRoutesFromCache() {
+        routes.postValue(
+            ServerRouteManager
+                .getRouteCache()
+                .filter { routeClassification.filter(it) }
+                .toTypedArray()
+        )
+    }
 
-        ServerRouteManager.provideRoutesByLocation(
+    private suspend fun cacheAllRoutes() {
+        loadCreatedRoutes()
+        loadFavouriteRoutes()
+        loadToDoRoutes()
+        loadNearbyRoutes()
+        loadRandomRoutes()
+    }
+
+    fun updateLoading() {
+        _isLoading.postValue(loadingScore > 0)
+    }
+
+    private suspend fun loadCreatedRoutes() {
+        val createdRoutesList = ServerRouteManager.getCreatedRoutesList(ServerUserManager.getSelfUser()?.username ?: "")
+
+        for (routeUID in createdRoutesList) {
+            loadingScore += 1
+            viewModelScope.launch {
+                ServerRouteManager.getRouteByID(routeUID)
+                loadingScore -= 1
+            }
+        }
+    }
+
+    private suspend fun loadFavouriteRoutes() {
+        val favouriteRoutesList = ServerUserManager.getSelfUser()?.featuredRoutes ?: listOf()
+        for (routeUID in favouriteRoutesList) {
+            loadingScore += 1
+            viewModelScope.launch {
+                ServerRouteManager.getRouteByID(routeUID)
+                loadingScore -= 1
+            }
+        }
+    }
+
+    private suspend fun loadToDoRoutes() {
+        val todoRoutesList = ServerUserManager.getSelfUser()?.toDoRoutes ?: listOf()
+        for (routeUID in todoRoutesList) {
+            loadingScore += 1
+            viewModelScope.launch {
+                ServerRouteManager.getRouteByID(routeUID)
+                loadingScore -= 1
+            }
+        }
+    }
+
+    private suspend fun loadNearbyRoutes() {
+        val nearbyRoutesList = ServerRouteManager.getLocatedRoutesList(
             WikihonkLocationManager.userLocation.value?.latitude ?: 0.0,
-            WikihonkLocationManager.userLocation.value?.longitude ?: 0.0,
-        ) {
-            this.addRoute(it)
-        }
-
-        ServerRouteManager.provideRandomRoutes {
-            this.addRoute(it)
-        }
-
-    }
-
-    fun requestMoreRoutes() {
-        // TODO request more
-    }
-
-    private fun addRoute(route: Route) {
-        routeQueue.add(route)
-    }
-
-    private fun checkRouteQueue() {
-        if (routeQueue.isNotEmpty()) {
-            _routes.value = _routes.value?.plus(routeQueue) ?: routeQueue.toTypedArray()
-
-            routeQueue.removeAll { true }
+            WikihonkLocationManager.userLocation.value?.longitude ?: 0.0
+        )
+        for (routeUID in nearbyRoutesList) {
+            loadingScore += 1
+            viewModelScope.launch {
+                ServerRouteManager.getRouteByID(routeUID)
+                loadingScore -= 1
+            }
         }
     }
+
+    private suspend fun loadRandomRoutes() {
+        val randomRoutesList = ServerRouteManager.getRandomRoutesList()
+        for (routeUID in randomRoutesList) {
+            loadingScore += 1
+            viewModelScope.launch {
+                ServerRouteManager.getRouteByID(routeUID)
+            }
+            loadingScore -= 1
+        }
+    }
+
+}
+
+enum class RouteClassification(
+    val value: String,
+    val filter: (route: Route) -> Boolean = { true }
+) {
+    ALL(
+        "all"
+    ),
+    FAVOURITE(
+        "favourite",
+        { ServerUserManager.isFavouriteRoute(it.uid) }
+    ),
+    TODO(
+        "todo",
+        { ServerUserManager.isToDoRoute(it.uid) }
+    ),
+    CREATED(
+        "created",
+        {  (ServerUserManager.getSelfUserOrNull()?.username ?: "").equals(it.creator) }
+    );
 
 }
