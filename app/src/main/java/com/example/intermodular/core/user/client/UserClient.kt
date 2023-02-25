c2dd4be1f93d2617e12ac9eeb91b51838e7a4d40
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-02-25 20:57:34 +0100

    implementado método de modificación de usuario

--- app/src/main/java/com/example/intermodular/core/user/client/UserClient.kt
+++ app/src/main/java/com/example/intermodular/core/user/client/UserClient.kt
@@ -1,8 +1,11 @@
 package com.example.intermodular.core.user.client
 
+import com.example.intermodular.core.user.model.ServerModifiedUser
 import com.example.intermodular.core.user.model.ServerUser
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface UserClient {
@@ -12,5 +15,10 @@ interface UserClient {
 
     @GET("/api/v1/user")
     suspend fun getSelfUser(): Response<ServerUser>
+
+    @POST("/account/modify")
+    suspend fun modifySelfUser(
+        @Body user: ServerModifiedUser
+    ): Response<Unit>
 
 }
